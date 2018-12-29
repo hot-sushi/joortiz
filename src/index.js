f9bd19c22133ab045b0eb0f72f1df0870c21a960
joortiz <joortiz@adobe.com>
2018-12-28 16:23:01 -0800

    basic ui configured

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import {
   Dropdown,
   Grid,
   Header,
+  Icon,
   Image,
   List,
   Menu,
@@ -16,20 +17,33 @@ import {
 import 'semantic-ui-css/semantic.min.css';
 import gmboi from './gmboi.png'
 
-const threm = { paddingTop: '3rem' }
+const threm = { marginTop: '4.88rem' }
 
 const Home = () => {
     return(
         <div style={threm}>
-            <h1>Home</h1>
+            <Grid container columns={12}>
+                <Grid.Row>
+                    <Grid.Column>
+                        <h1>Home</h1>
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
         </div>
     );
 }
 
 const About = () => {
     return(
+
         <div style={threm}>
-            <h1>About</h1>
+            <Grid container columns={12}>
+                <Grid.Row>
+                    <Grid.Column>
+                        <h1>About</h1>
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
         </div>
     );
 }
@@ -38,28 +52,36 @@ const About = () => {
 
 const Topics = () => {
     return(
+
         <div style={threm}>
-            <h1>Topics</h1>
+            <Grid container columns={12}>
+                <Grid.Row>
+                    <Grid.Column>
+                        <h1>Skills</h1>
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
         </div>
     )
 }
 
 const Navigation = () => (
     <div>
-    <Menu fixed='top' inverted>
-      <Container>
-        <Menu.Item header>
-          <Image size='mini' src={gmboi} style={{ marginRight: '1.5em' }} />
-          Jonathan Ortiz
-        </Menu.Item>
-        <Menu.Item><Link to="/joortiz">Home</Link></Menu.Item>
-        <Menu.Item><Link to="/about">About</Link></Menu.Item>
-        <Menu.Item><Link to="/topics">Topics</Link></Menu.Item>
-      </Container>
-    </Menu>
-    <Route exact path="/joortiz" component={Home} />
-    <Route path="/about" component={About} />
-    <Route path="/topics" component={Topics} />
+        <Menu fixed='top' inverted>
+            <Container>
+                <Menu.Item header>
+                <Image size='mini' src={gmboi} style={{margin:'0 1.4rem .333rem 0'}}/>
+                joortiz
+                </Menu.Item>
+                <Menu.Item><Link to="/joortiz"><Icon name='home' size='large'/></Link></Menu.Item>
+                <Menu.Item><Link to="/about"><Icon name='user circle outline' size='large'/></Link></Menu.Item>
+                <Menu.Item><Link to="/topics"><Icon name='code branch' size='large'/></Link></Menu.Item>
+            </Container>
+        </Menu>
+        <Route exact path="/" component={Home} />
+        <Route path="/joortiz" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/topics" component={Topics} />
     </div>
 );
 
@@ -68,12 +90,9 @@ const Navigation = () => (
 class App extends Component {
     render () {
         return(
-            <Router>
-                <Container fluid>
+                <Router>
                     <Navigation />
-                </Container>
-            </Router>
-
+                </Router>
         )
     }
 }
